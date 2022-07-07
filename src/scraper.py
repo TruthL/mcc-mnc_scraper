a985d7fb11ad1c766da2484997d9724365a9afe7
TruthL <jinreelee@gmail.com>
2022-07-07 23:43:19 +0200

    scraper case added

--- src/scraper.py
+++ src/scraper.py
@@ -2,17 +2,21 @@ import sys
 import utils.func as func
 import re
 import formulate as form
-import update as up
+import download as down
 
 # 1 = FORMULATE
-# 2 = BULLETIN VARIFICATION & UPDATE
+# 2 = BULLETIN DOWNLOAD
+# 3 = APPLICATION OF BULLETINS
 def main(case):
     if case == 1:
         form.formulate()
     elif case == 2:
-        up.bulletin()
+        down.bulletin()
+    elif case == 3:
+        print('apply')
     else:
-        usage = 'python3 scraper.py (case)\n case:\n 1 = Formulate table\n 2 = Update'
+        usage = 'python3 scraper.py (case)\n case:\n 1 = Formulate table\n \
+        2 = download bulletins \n 3 = apply bulletins'
         print('usage : '+ usage)
 
 
